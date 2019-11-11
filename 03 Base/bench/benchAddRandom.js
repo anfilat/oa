673a0ec41b0bb4125408e2999c27275058b14008
Andrey Filatkin <anfilat@mail.ru>
2019-11-11 20:54:37 +0300

    Доразбил бенчмарки

--- 03 Base/bench/benchAddRandom.js
+++ 03 Base/bench/benchAddRandom.js
@@ -9,46 +9,46 @@ const { TypedF64Array } = require('../typedF64Array');
 [100, 1000, 10000].map(bench);
 
 function bench(arrSize) {
-    new Benchmark.Suite('Вставка в начало')
+    new Benchmark.Suite('Случайная вставка')
         .add('SingleArray', function () {
             const arr = new SingleArray();
             for (let i = 0; i < arrSize; i++) {
-                arr.add(0, 0);
+                arr.add(0, arr.length * Math.random() | 0);
             }
         })
         .add('VectorArray', function () {
             const arr = new VectorArray();
             for (let i = 0; i < arrSize; i++) {
-                arr.add(0, 0);
+                arr.add(0, arr.length * Math.random() | 0);
             }
         })
         .add('FactorArray', function () {
             const arr = new FactorArray();
             for (let i = 0; i < arrSize; i++) {
-                arr.add(0, 0);
+                arr.add(0, arr.length * Math.random() | 0);
             }
         })
         .add('MatrixArray', function () {
             const arr = new MatrixArray();
             for (let i = 0; i < arrSize; i++) {
-                arr.add(0, 0);
+                arr.add(0, arr.length * Math.random() | 0);
             }
         })
         .add('SystemArray', function () {
             const arr = new SystemArray();
             for (let i = 0; i < arrSize; i++) {
-                arr.add(0, 0);
+                arr.add(0, arr.length * Math.random() | 0);
             }
         })
         .add('TypedF64Array', function () {
             const arr = new TypedF64Array();
             for (let i = 0; i < arrSize; i++) {
-                arr.add(0, 0);
+                arr.add(0, arr.length * Math.random() | 0);
             }
         })
         .on('cycle', function (event) {
             global.gc();
-            console.log('Вставка в начало', arrSize, String(event.target));
+            console.log('Случайная вставка', arrSize, String(event.target));
         })
         .run();
 }
